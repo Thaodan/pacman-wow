ff4909c49e477d65ebcbe3997e0e1b696e9d2815
Björn Bidar <theodorstormgrade@gmail.com>
2013-10-29 18:33:42 +0100

    updated

--- Provider/Blizzard.py
+++ Provider/Blizzard.py
@@ -4,64 +4,48 @@ Moddata Provider to read data from TocFile
 
 '''
 
-config = CONFIG_DIR + blizzard.cfg
+config = CONFIG_DIR + 'blizzard.cfg'
 
 Type = "Local" # Provider represents some parts of our Local Data
-__self__     = Provider.Blizzard
 class Mod:
     ''' Read Moddata from Blizzard TocFile, contains standard methods to return imformations like other Provider'''
-    def __init__():
-	TocFile = TocFile(Shortname)
-    def Name(Url):
-	return TocFile.ReadField('Title')
-    def Description(Url):
-	return TocFile.ReadField('Notes')
-    def Version():
-	return TocFile.ReadField('Version')
-    def ProjectUrl(Url):
-	return TocFile.ReadField('X-Website')
-    def Url(shortname):
-	return False
-    def DownloadUrl(Url):
-	return False # not aviable this provider only provides data
-    def DownloadUrlL(DownloadUrls):
-	return False # same 
-    def RequiredDepencies():
-	__required_depencies__ = []
+    def __init__(self, shortname):
+        self.TocFile = TocFile(shortname)
+    def Name(self):
+        return self.TocFile.ReadField('Title')
+    def Description(self):
+        return self.TocFile.ReadField('Notes')
+    def Version(self):
+        return self.TocFile.ReadField('Version')
+    def ProjectUrl(self):
+        return self.TocFile.ReadField('X-Website')
+    def Url(self):
+        return False
+    def DownloadUrl(self):
+        return False # not aviable this provider only provides data
+    def DownloadUrlL(self):
+        return False # same 
+    def RequiredDepencies(self):
+        __required_depencies__ = []
 	# FIXME: replace with cleaner code
 	# see: www.wowpedia.org/The_TOC_Format#Depencies
-	try:
-	    __required_depencies__ = TocFile.ReadField('Require dDeps')
-	except:
-	    try: 
-		__required_depencies__ = TocFile.ReadField('Depencies')
-	    except:
-		try:
-		    __required_depencies__ = TocFile.ReadField('Deps')
-		except:
-		    return False
+        try:
+            __required_depencies__ = self.TocFile.ReadField('Require dDeps')
+        except:
+            try: 
+                __required_depencies__ = self.TocFile.ReadField('Depencies')
+            except:
+                try:
+                    __required_depencies__ = self.TocFile.ReadField('Deps')
+                except:
+                    return False
 
-	return __required_depencies__
+            return __required_depencies__
     
-    def OptionalDepencies():
-	return TocFile.ReadField('OptionalDeps')
+    def OptionalDepencies(self):
+        return self.TocFile.ReadField('OptionalDeps')
     def Add():
-	return False
+        return False
     def Remove():
-	return False
+        return False
 
-class DB:
-    def __init__():
-	pass
-    def Fetch():
-	pass
-    def Update(source_db):
-	return True
-    def SyncStatus():
-	return False
-    def LastAcces():
-	return True
-    def LastModifed():
-	return True
-    def SourceProvider():
-	return __self__.Name
